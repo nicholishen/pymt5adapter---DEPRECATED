4995383317d3fa5618c095372a1048f25dd0a605
nicholishen <nickerbocker@tuta.io>
2020-04-02 11:44:22 -0400

    Woops.... let's let MQ handle the code on these. Just passing through and providing some sweet type hinting.

--- pymt5adapter/oem.py
+++ pymt5adapter/oem.py
@@ -1,88 +1,31 @@
-from .const import *
-from .core import *
+import MetaTrader5 as _mt5
+
 from .types import *
 
+
 # internal order send
-def _RawOrder(order_type:int,
-              symbol:str,
-              volume:float,
-              price:float,
-              comment:str=None,
-              ticket:int=None
+def _RawOrder(order_type: int,
+              symbol: str,
+              volume: float,
+              price: float,
+              comment: str = None,
+              ticket: int = None
               ) -> OrderSendResult:
-    order = {
-        "action"   : TRADE_ACTION_DEAL,
-        "symbol"   : symbol,
-        "volume"   : volume,
-        "type"     : order_type,
-        "price"    : price,
-        "deviation": 10,
-    }
-    if comment != None:
-        order["comment"] = comment
-    if ticket != None:
-        order["position"] = ticket
-    r = order_send(order)
-    return r
+    return _mt5._RawOrder(order_type, symbol, volume, price, comment, ticket)
 
 
 # Close all specific orders
-def Close(symbol:str, *, comment:str=None, ticket:int=None) -> Union[bool, str]:
-    if ticket != None:
-        positions = positions_get(ticket=ticket)
-    else:
-        positions = positions_get(symbol=symbol)
-
-    tried = 0
-    done = 0
-
-    for pos in positions:
-        # process only simple buy, sell
-        if pos.type == ORDER_TYPE_BUY or pos.type == ORDER_TYPE_SELL:
-            tried += 1
-            for tries in range(10):
-                info = symbol_info_tick(symbol)
-                if pos.type == ORDER_TYPE_BUY:
-                    r = _RawOrder(ORDER_TYPE_SELL, symbol, pos.volume, info.bid, comment, pos.ticket)
-                else:
-                    r = _RawOrder(ORDER_TYPE_BUY, symbol, pos.volume, info.ask, comment, pos.ticket)
-                # check results
-                if r.retcode != TRADE_RETCODE_REQUOTE and r.retcode != TRADE_RETCODE_PRICE_OFF:
-                    if r.retcode == TRADE_RETCODE_DONE:
-                        done += 1
-                    break
-
-    if done > 0:
-        if done == tried:
-            return True
-        else:
-            return "Partially"
-    return False
+def Close(symbol: str, *, comment: str = None, ticket: int = None) -> Union[bool, str]:
+    return _mt5.Close(symbol, comment=comment, ticket=ticket)
 
 
 # Buy order
-def Buy(symbol:str, volume:float, price:float=None, *, comment:str=None, ticket:int=None) -> OrderSendResult:
-    # with direct call
-    if price != None:
-        return _RawOrder(ORDER_TYPE_BUY, symbol, volume, price, comment, ticket)
-    # no price, we try several times with current price
-    for tries in range(10):
-        info = symbol_info_tick(symbol)
-        r = _RawOrder(ORDER_TYPE_BUY, symbol, volume, info.ask, comment, ticket)
-        if r.retcode != TRADE_RETCODE_REQUOTE and r.retcode != TRADE_RETCODE_PRICE_OFF:
-            break
-    return r
+def Buy(symbol: str, volume: float, price: float = None, *,
+        comment: str = None, ticket: int = None) -> OrderSendResult:
+    return _mt5.Buy(symbol, volume, price, comment=comment, ticket=ticket)
 
 
 # Sell order
-def Sell(symbol:str, volume:float, price:float=None, *, comment:str=None, ticket:int=None) -> OrderSendResult:
-    # with direct call
-    if price != None:
-        return _RawOrder(ORDER_TYPE_SELL, symbol, volume, price, comment, ticket)
-    # no price, we try several times with current price
-    for tries in range(10):
-        info = symbol_info_tick(symbol)
-        r = _RawOrder(ORDER_TYPE_SELL, symbol, volume, info.bid, comment, ticket)
-        if r.retcode != TRADE_RETCODE_REQUOTE and r.retcode != TRADE_RETCODE_PRICE_OFF:
-            break
-    return r
+def Sell(symbol: str, volume: float, price: float = None, *,
+         comment: str = None, ticket: int = None) -> OrderSendResult:
+    return _mt5.Sell(symbol, volume, price, comment=comment, ticket=ticket)
